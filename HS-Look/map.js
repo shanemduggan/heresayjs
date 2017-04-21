cb54a88d48bae62f619caf163abeed1ebe8935b3
sduggan <shane@zefr.com>
2017-04-20 20:59:17 -0700

    refining heresay look - inspiration from livemapua

---
+++ HS-Look/map.js
@@ -0,0 +1,11 @@
+// function initMap() {
+	// var losAngeles = {
+		// lat : 34.0416,
+		// lng : -118.328661
+	// }
+	// map = new google.maps.Map(document.getElementById('map_canvas'), {
+		// zoom : 11,
+		// center : losAngeles,
+		// mapTypeControl : false
+	// });
+// }
